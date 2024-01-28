72f7e2594f008a107de1640d66636ac2815185b9
Zichqec <36972872+Zichqec@users.noreply.github.com>
2024-01-28 01:27:23 -0500

    StartUp urls

--- interface.js
+++ interface.js
@@ -150,18 +150,18 @@ document.getElementById('interface_header').outerHTML = `
 <br /></li></ul></li>
 <li>Getting started
 <ul class="list2 list-indent1"><li><a href="StartUp" title="StartUp" class="link_page_passage">What is AYA/YAYA?</a></li>
-<li><a href="StartUp/YAYA%E3%81%A7%E3%82%B4%E3%83%BC%E3%82%B9%E3%83%88%E3%82%92%E4%BD%9C%E3%82%8B" title="StartUp/YAYAでゴーストを作る" class="link_page_passage">Creating a new ghost</a></li>
-<li><a href="StartUp/AYA%E3%81%8B%E3%82%89%E3%81%AE%E7%A7%BB%E8%A1%8C" title="StartUp/AYAからの移行" class="link_page_passage">Convert from AYA</a></li>
-<li><a href="StartUp/%E9%87%8C%E3%80%85%E3%81%8B%E3%82%89%E3%81%AE%E7%A7%BB%E8%A1%8C" title="StartUp/里々からの移行" class="link_page_passage">Convert from Satori</a>
+<li><a href="startup/making_ghosts_with_yaya.html" title="StartUp/Making ghosts with YAYA" class="link_page_passage">Making ghosts with YAYA</a></li>
+<li><a href="startup/migrate_from_aya.html" title="StartUp/Migrate from AYA" class="link_page_passage">Migrate from AYA</a></li>
+<li><a href="startup/migrate_from_satori.html" title="StartUp/Migrate from Satori" class="link_page_passage">Migrate from Satori</a>
 <br />
 <br /></li></ul></li>
 <li><a href="%E3%83%9E%E3%83%8B%E3%83%A5%E3%82%A2%E3%83%AB" class="link_page_passage">Manual</a>
 <ul class="list2 list-indent1"><li><a href="%E3%83%9E%E3%83%8B%E3%83%A5%E3%82%A2%E3%83%AB/%E5%9F%BA%E6%9C%AC"class="link_page_passage">Basic</a></li>
 <li><a href="%E3%83%9E%E3%83%8B%E3%83%A5%E3%82%A2%E3%83%AB/%E6%96%87%E6%B3%95"class="link_page_passage">Grammar</a></li>
 <li><a href="%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E8%BE%9E%E6%9B%B8" class="link_page_passage">System dictionary</a></li>
-<li><a href="manual/function/list_of_functions_by_purpose" title="Manual/List of functions by purpose" class="link_page_passage">List of functions by application</a></li>
+<li><a href="manual/function/list_of_functions_by_purpose.html" title="Manual/List of functions by purpose" class="link_page_passage">List of functions by application</a></li>
 <li><a href="manual/function" title="Manual/Function" class="link_page_passage">List of functions by name</a></li>
-<li><a href="%E3%83%9E%E3%83%8B%E3%83%A5%E3%82%A2%E3%83%AB/%E3%82%A8%E3%83%A9%E3%83%BC%E5%87%A6%E7%90%86%E9%96%A2%E6%95%B0" title="マニュアル/エラー処理関数" class="link_page_passage">Error-handling functions</a>
+<li><a href="manual/error_handling_function.html" title="Manual/Error Handling Function" class="link_page_passage">Error-handling functions</a>
 <br />
 <br /></li></ul></li>
 <li><a href="Tips" class="link_page_passage">Tips</a>
@@ -202,7 +202,7 @@ document.getElementById('interface_header').outerHTML = `
  <li><a href="manual/function/GETTYPEEX" class="link_page_passage">Manual/Function/GETTYPEEX</a></li>
  <li><a href="%E3%83%9E%E3%83%8B%E3%83%A5%E3%82%A2%E3%83%AB/%E5%9F%BA%E6%9C%AC" class="link_page_passage">マニュアル/基本</a></li>
  <li><a href="wiki%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6%E3%81%AE%E4%BC%9D%E8%A8%80%E6%9D%BF" class="link_page_passage">wikiについての伝言板</a></li>
- <li><a href="manual/function/list_of_functions_by_purpose" class="link_page_passage">Manual/List of functions by purpose</a></li>
+ <li><a href="manual/function/list_of_functions_by_purpose.html" class="link_page_passage">Manual/List of functions by purpose</a></li>
  <li><a href="manual/function/HAN2ZEN" class="link_page_passage">Manual/Function/HAN2ZEN</a></li>
 </ul>
 </div>
